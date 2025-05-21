889d5c1fcf9692d1d2f62cf436a192517ca7d4d8
ap3ufersa <ap3ufersa@gmail.com>
2025-05-21 10:55:04 -0300

    Arrumando aula 3

--- ap3_2025.1_xicoArruda/unidade1/gabaritos/exercicio2/MediaProgOO.java
+++ ap3_2025.1_xicoArruda/unidade1/gabaritos/exercicio2/MediaProgOO.java
@@ -4,7 +4,7 @@ public class MediaProgOO {
 
     public static void main(String[] args) {
         Aluno xico = new Aluno();
-		xico.setNota1(?);
+        xico.setNota1(?);
                 
         System.out.println();
         System.out.println(xico);
--- ap3_2025.1_xicoArruda/unidade1/gabaritos/exercicio2/TestaAluno.java
+++ ap3_2025.1_xicoArruda/unidade1/gabaritos/exercicio2/TestaAluno.java
@@ -4,9 +4,9 @@ public class TestaAluno {
     public static void main(String[] args) {
 
         System.out.println();
-        System.out.println("Um aluno aprovado:");
-
+        
         // um aluno
+        System.out.println("Um aluno aprovado:");
         Aluno xico = new Aluno();
         xico.setNome("Xico Arruda");
         System.out.println(xico);
@@ -15,7 +15,7 @@ public class TestaAluno {
         System.out.println("Muitos alunos, notas aleatórias:");
         for (int i = 0; i < 99; i++) {
             Aluno umAluno = new Aluno();
-            umAluno.setNome("AlunoNumero" + i);
+            umAluno.setNome("AlunoNumero=" + i);
             Random random = new Random();
             umAluno.set...
             System.out.println(umAluno);
